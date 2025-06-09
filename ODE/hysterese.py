ed7ad9da6f0135dec2dfcd1f392b29344cf95c53
huyenanh-nguyen <ng_huyenanh@mailbox.org>
2025-06-09 10:27:27 +0200

    hystersis with row and column

--- ODE/hysterese.py
+++ ODE/hysterese.py
@@ -26,6 +26,10 @@ alpha_down = alpha_up[::-1]
 
 def compute_amplitude(par, t, keep, k, mu, gamma, alpha, beta):
     amp = np.mean(OnesidedCoupling(par, t, keep, k, mu, gamma, alpha, beta).find_peaks_max()[1][1]['peak_heights'][-10:])
+    # if math.isnan(amp):
+    #     return 0
+    
+    # else:
     return amp
 
 
@@ -50,64 +54,57 @@ def compute_amplitude(par, t, keep, k, mu, gamma, alpha, beta):
 #     amplitudes_down.append(compute_amplitude(par0, t, keep, k, mu, gamma, j, beta))
 
 
-# plt.plot(np.sqrt(alpha_up), amplitudes_up, label="Increasing ω")
-# plt.plot(np.sqrt(alpha_down), amplitudes_down, label="Decreasing ω")
-# plt.xlabel("$\omega _0$ in Hz", fontsize = 30)
-# plt.ylabel("A in a.u.", fontsize = 30)
-# plt.xticks(fontsize = 20)
-# plt.yticks(fontsize = 20)
-# plt.legend(fontsize = 30)
+# plt.plot(np.sqrt(alpha_up), amplitudes_up, label="Increasing ω", color = "#283D3B")
+# plt.plot(np.sqrt(alpha_down), amplitudes_down, label="Decreasing ω", color = "#F8D794")
+# plt.xlabel("$\omega _0$ in Hz", fontsize = 25)
+# plt.ylabel("A in a.u.", fontsize = 25)
+# plt.axvline(x = np.sqrt(0.1), color = "r")
+# plt.xticks(fontsize = 18)
+# plt.yticks(fontsize = 18)
+# plt.legend(fontsize = 16)
+# plt.savefig("Hysteresis_withvertikalline" + ".png", dpi =  300, bbox_inches = "tight")
 # plt.show()
 
 
 # [Basin of Attractors]______________________________________________________________________________________________________
 
-alph = [0.05, 0.1, 0.15, 0.2]
+alph = [0.1]
+
 
 colours = ["#283D3B", "#F8D794"]
 
 cmap = ListedColormap(colours)
 for i in range(len(alph)):
-    x_par = np.arange(-2,2,0.1)
-    y_par = np.arange(-2,2,0.1)
-    p_par = np.arange(-2,2,0.1)
-    q_par = np.arange(-2,2,0.1)
+    x_par = np.arange(-3,3,0.1)
+    y_par = np.arange(-3,3,0.1)
+    p_par = np.arange(-3,3,0.1)
+    q_par = np.arange(-3,3, 0.1)
   
 
-    y,p = np.meshgrid(y_par,p_par)
+    y,p = np.meshgrid(y_par,q_par)
     attractor = np.zeros_like(y)
     y_amplitude_matrix = np.zeros_like(y)
-    par0 = -1, -1, -1, -1
 
-    sol = max(OnesidedCoupling(par0, t, keep, k, mu, gamma, alph[i], beta).duffvdpsolver()[-keep:, 1])
+    for l in range(len(q_par)):
+        for m in range(len(y_par)):
 
-    for l in range(len(y_par)):
-        for m in range(len(p_par)):
-
-            par0 = [x_par[l], y_par[m], p_par[l], q_par[m]]
+            par0 = [x_par[m], y_par[m], p_par[l], q_par[l]]
             y_amplitude = compute_amplitude(par0, t, keep, k, mu, gamma, alph[i], beta)
             y_amplitude_matrix[l,m] = round(y_amplitude, 2)
-            
-            if math.isclose(y_amplitude, sol, rel_tol=1e-1):
-                attractor[l,m] = 1
-
-            
-            else: 
-                attractor[l,m] = 0
     
-    print(sol)
+    # print(sol)
     print(y_amplitude_matrix)
     
-    plt.imshow(attractor, extent=[-2,2,-2,2], cmap = cmap)
-    plt.xlabel("y in a.u.",fontsize = 30)
-    plt.ylabel("q in a.u.",fontsize = 30)
+    plt.imshow(y_amplitude_matrix, extent=[-3,3,-3,3], cmap = cmap)
+    plt.xlabel("y in a.u.",fontsize = 25)
+    plt.ylabel("q in a.u.",fontsize = 25)
     plt.title(label = "$\\alpha$ = " + f"{alph[i]:.4f}" + ", $\\omega$ = " + f"{np.sqrt(alph[i]):.4f}", fontsize = 20)
-    plt.xticks(np.linspace(-2,2, 5), fontsize = 20)
-    plt.yticks(np.linspace(-2,2, 5),fontsize = 20)
+    plt.xticks(np.linspace(-3,3, 7), fontsize = 18)
+    plt.yticks(np.linspace(-3,3, 7),fontsize = 18)
     
-    labels = [f"A = {attractor.min():.2f}", f"A = {attractor.max():.2f}"]
+    labels = ["$\overline{A}_{10}$ =" + f"{np.nanmin(y_amplitude_matrix):.2f}", "$\overline{A}_{10}$ =" + f"{np.nanmax(y_amplitude_matrix):.2f}"]
     patches = [mpatches.Patch(color=colours[i], label=labels[i]) for i in range(len(colours))]
-    plt.legend(handles=patches, loc='upper right', fontsize = 16)
+    plt.legend(handles=patches, loc='upper right', fontsize = 14)
     
-    plt.savefig("Basin_detailed" + f"{i}" + ".png", dpi =  300, bbox_inches = "tight")
+    plt.savefig("Basin_detailed" + str(i) + ".png", dpi =  300, bbox_inches = "tight")
             
